8b6d2feb175980f6d66cd5165b1a22fd704074ca
Cyriel Basilio <cbasilyow@gmail.com>
2023-06-19 23:08:47 +0800

    Add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [counter, setCounter] = useState(0)
+
+	const handleIncrement = () => {
+		setCounter((counter += 1))
+	}
+
+	const handleDecrement = () => {
+		setCounter((counter -= 1))
+	}
+
+	return (
+		<div className="App">
+			<h6>{`Counter: ${counter}`}</h6>
+			<div className="button-container">
+				<button onClick={handleIncrement}>Increment</button>
+				<button disabled={counter <= 0} onClick={handleDecrement}>
+					Decrement
+				</button>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
